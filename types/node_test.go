a2284656ab599d1235b31af32dc8f12754b78b2e
Hadrien Dorio <hadrien.dorio@gmail.com>
2015-07-02 13:03:15 +0200

    add test case for CypherNode

--- types/node_test.go
+++ types/node_test.go
@@ -30,3 +30,26 @@ func (s *TypesSuite) TestQueryNode(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(labels, DeepEquals, []string{"Test"})
 }
+
+func (s *TypesSuite) TestQueryNodeWithStringPropertiesOnly(c *C) {
+	testURL := "http://neo4j:test@localhost:7474/"
+	conn, err := sql.Open("neo4j-cypher", testURL)
+	c.Assert(err, IsNil)
+	stmt, err := conn.Prepare(`create (a:Test {foo:"bar", bar: "foo"}) return a`)
+	c.Assert(err, IsNil)
+	rows, err := stmt.Query()
+	c.Assert(err, IsNil)
+
+	rows.Next()
+	var test types.Node
+	err = rows.Scan(&test)
+	c.Assert(err, IsNil)
+	t1 := types.Node{}
+	t1.Properties = map[string]types.CypherValue{}
+	t1.Properties["foo"] = types.CypherValue{types.CypherString, "bar"}
+	t1.Properties["bar"] = types.CypherValue{types.CypherString, "foo"}
+	c.Assert(test.Properties, DeepEquals, t1.Properties)
+	labels, err := test.Labels(testURL)
+	c.Assert(err, IsNil)
+	c.Assert(labels, DeepEquals, []string{"Test"})
+}
