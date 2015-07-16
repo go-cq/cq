afe98cce9f14d2c2d42a616e510d679312c407a0
Jeff <jgregory@bitcasa.com>
2015-07-16 13:10:50 -0700

    add test for node with only one property

--- types/node_test.go
+++ types/node_test.go
@@ -30,3 +30,25 @@ func (s *TypesSuite) TestQueryNode(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(labels, DeepEquals, []string{"Test"})
 }
+
+func (s *TypesSuite) TestQueryNodeWithOneProperty(c *C) {
+	testURL := "http://neo4j:test@localhost:7474/"
+	conn, err := sql.Open("neo4j-cypher", testURL)
+	c.Assert(err, IsNil)
+	stmt, err := conn.Prepare(`create (a:Test {i:1}) return a`)
+	c.Assert(err, IsNil)
+	rows, err := stmt.Query()
+	c.Assert(err, IsNil)
+
+	rows.Next()
+	var test types.Node
+	err = rows.Scan(&test)
+	c.Assert(err, IsNil)
+	t1 := types.Node{}
+	t1.Properties = map[string]types.CypherValue{}
+	t1.Properties["i"] = types.CypherValue{types.CypherInt, 1}
+	c.Assert(test.Properties, DeepEquals, t1.Properties)
+	labels, err := test.Labels(testURL)
+	c.Assert(err, IsNil)
+	c.Assert(labels, DeepEquals, []string{"Test"})
+}
