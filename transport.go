640d1301f2428a200759b8777527a7d77737f85e
Valentin Tjoncke <valtjo@gmail.com>
2016-07-19 16:45:04 +0200

    Gofmt cleaning

--- transport.go
+++ transport.go
@@ -1,19 +1,19 @@
 package cq
 
 import (
-  "net/http"
-  "github.com/origininvesting/cq/types"
+	"github.com/origininvesting/cq/types"
+	"net/http"
 )
 
 var (
-  transport http.RoundTripper = &http.Transport{}
-  client = &http.Client{
-    Transport: transport,
-  }
+	transport http.RoundTripper = &http.Transport{}
+	client                      = &http.Client{
+		Transport: transport,
+	}
 )
 
 func SetTransport(rt http.RoundTripper) {
 	transport = rt
 	client.Transport = transport
-  types.SetTransport(rt)
+	types.SetTransport(rt)
 }
